9272347d27655e84fa9f708ab2cf2c6a0247708e
macoyshev <make.oynoshev@gmail.com>
2022-03-09 19:06:07 +0300

    final

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# Python Template
-
-
-## CLI
+# WeatherRU
+The console application that allows 
+to find out the weather in any city in Russia.
+Requests are cached, which allows to quickly
+get up-to-date weather information. 
 ### Running application
+To run the application execute the following command:
 ```sh
-
+$ python -m weather city
 ```
+Where __*city*__ is an arbitrary city in Russia, as in example:
+
+![img.png](images/img.png)
 
 ##### Requirements
-You must have Python 3.8+ to run the game.
+You must have `Python 3.8+` and
+the following external libraries:
+- `typer `
+- `bs4`
+- `requests`
+
--- tests/weather/test_weather_parser.py
+++ tests/weather/test_weather_parser.py
@@ -4,22 +4,26 @@ from weather.weather_parser import get_weather_for_city
 
 
 @pytest.fixture()
-def request_mock(mocker):
+def req_mock(mocker):
     return mocker.patch('weather.weather_parser.requests.get')
 
 
 @pytest.mark.parametrize('city, expected_value', [('kazan', '-999°'),
                                                   ('moscow', '-1000°'),
                                                   ('orel', '+12°')])
-def test_right_city(request_mock, city, expected_value):
-    request_mock.return_value.text = f'<div id="weather-now-number">{expected_value}</div>'
-    request_mock.return_value.status_code = 200
+def test_right_city(req_mock, city, expected_value):
+    req_mock.return_value.text = f'<div id="weather-now-number">{expected_value}</div>'
+    req_mock.return_value.status_code = 200
 
     assert expected_value == get_weather_for_city(city)
 
 
-def test_wrong_city(request_mock):
-    request_mock.return_value.status_code = 404
+def test_wrong_city(req_mock):
+    req_mock.return_value.status_code = 404
 
     with pytest.raises(RuntimeError):
         get_weather_for_city('iphone')
+
+
+def test_1():
+    assert 1 == 1
